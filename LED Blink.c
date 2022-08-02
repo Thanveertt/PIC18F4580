f666460fc59ff1ae2a68b4c5c68e8a04b696cb4f
Thanveertt <109418681+Thanveertt@users.noreply.github.com>
2022-08-02 11:33:26 +0530

    Update and rename LED.c to LED Blink.c

--- LED Blink.c
+++ LED Blink.c
@@ -5,15 +5,14 @@ void main()
     TRISB=0X00;
     while(1)
     {
-         PORTB=0Xff;
-         delay();
+        PORTB=0Xff;
+        delay();
         PORTB=0X00;
         delay();
     }  
 }
 void delay()
 {
-    int i,j;
+    int i;
     for(i=0;i<10000;i++);
-    //for(j=0;j<100;j++);
 }
